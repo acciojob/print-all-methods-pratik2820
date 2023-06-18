29ee5c4724ce4d56ca1c30fb9bdd4f963430848b
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-18 16:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	const methods = Object.getOwnPropertyNames(Math).filter(
+    (property) => typeof Math[property] === 'function'
+  );
+
+  console.log(methods);
 }
 
-alert(allMethods());
+// alert(allMethods());
